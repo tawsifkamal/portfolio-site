7c38d5a5a8f1e9dc938381ce08574815f3f7962b
tawsif <tawsifkamal123@gmail.com>
2026-01-28 12:44:02 -0800

    Remove untested code to establish coverage baseline

--- src/app/utils/untested-math.ts
+++
@@ -1,8 +0,0 @@
-// Untested utility - will fail patch coverage
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-export function subtract(a: number, b: number): number {
-  return a - b;
-}
