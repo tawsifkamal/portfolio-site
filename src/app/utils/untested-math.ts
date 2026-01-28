16c26b8ad563720d48f74b154713d04414ea148f
tawsif <tawsifkamal123@gmail.com>
2026-01-28 09:15:04 -0800

    Add test workflows and untested code for CI failure testing

---
+++ src/app/utils/untested-math.ts
@@ -0,0 +1,8 @@
+// Untested utility - will fail patch coverage
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
